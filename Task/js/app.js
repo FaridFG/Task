5d4402b433dc27bdad43131638b16c06bb0b0d3e
FaridFG <55425635+FaridFG@users.noreply.github.com>
2020-07-15 09:55:43 +0400

    whole page responsiveness added

--- Task/js/app.js
+++ Task/js/app.js
@@ -1,13 +1,13 @@
 $(function() {
     // function for showing the dropdown menu with slideDown() method
-    $("#dropdown").click(function() {
-        $(".left-menu > ul > li:last-child > ul").slideToggle(500);
+    $(".dropdown").click(function() {
+        $(this).parent().children("ul").fadeToggle(500);
     });
 
     // function for showing the modal window
     $(".btn-close").click(function() {
         $("div.modal").fadeIn("fast", function() {
-            $("div.modal .content").fadeIn("slow");
+            $("div.modal .content").fadeIn("fast");
             $("body").css({
                 "height": "100vh",
                 "overflow-y": "hidden"
@@ -15,7 +15,7 @@ $(function() {
         });
     });
     $(".close-modal").click(function() {
-        $("div.modal .content").fadeOut("slow", function() {
+        $("div.modal .content").fadeOut("fast", function() {
             $("div.modal").fadeOut("fast", function() {
                 $("body").css({
                     "height": "auto",
@@ -23,5 +23,24 @@ $(function() {
                 });
             });
         });
+    });
+
+    // showing responsive menu when screen width is less than 768px
+    let showButton = document.querySelector(".navbar nav .responsive-menu a");
+    showButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        $(".responsive-dropdown-menu").slideToggle();
+    });
+
+    // responsive sidebar
+    $(".responsive-sidebar > ul > li:first-child a").click(function(e) {
+        e.preventDefault();
+        if ($(this).children()[0].className === "fas fa-bars") {
+            $(this).children()[0].className = "fas fa-times";
+        } else {
+            $(this).children()[0].className = "fas fa-bars";
+        }
+        $(".responsive-sidebar").toggleClass("show-sidebar");
+        $(this).parent().siblings().toggleClass("hide-menu");
     });
 })
